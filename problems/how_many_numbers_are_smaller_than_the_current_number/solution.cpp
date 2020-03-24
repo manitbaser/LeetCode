0bb708491b71309dc70604900f5c8ae99124c1e4
Manit Baser <49650773+manitbaser@users.noreply.github.com>
2020-03-24 04:16:07 +0000

    Sync LeetCode submission - How Many Numbers Are Smaller Than the Current Number (cpp)

---
+++ problems/how_many_numbers_are_smaller_than_the_current_number/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        map<int,int> x;
+        for(int i = 0;i<nums.size();i++){
+            x[nums[i]]++;
+        }
+        // vector<int> ans;
+        int count = 0;
+        for(auto itr = x.begin();itr!=x.end();itr++){
+            int temp = count;
+            count+=itr->second;
+            itr->second = temp;
+        }
+        for(int i = 0;i<nums.size();i++){
+            nums[i] = x[nums[i]];
+        }
+        return nums;
+    }
+};
