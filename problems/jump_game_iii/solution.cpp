4b17f9d0516462a8e4705ce6ec7a06b58255f720
Manit Baser <49650773+manitbaser@users.noreply.github.com>
2020-06-07 10:22:56 +0000

    Sync LeetCode submission - Jump Game III (cpp)

---
+++ problems/jump_game_iii/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        if(start>arr.size()||start<0){
+            return false;
+        }
+        if(arr[start]==-1){
+            return false;
+        }
+        if(arr[start]==0){
+            return true;
+        }
+        int temp = arr[start];
+        arr[start] = -1;
+        return canReach(arr,start-temp)||canReach(arr,start+temp);
+    }
+};
