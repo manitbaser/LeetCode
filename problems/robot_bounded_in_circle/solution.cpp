8db68327c440d5bce832b26b44f2b14c14ab7b02
Manit Baser <49650773+manitbaser@users.noreply.github.com>
2020-06-07 10:05:15 +0000

    Sync LeetCode submission - Robot Bounded In Circle (cpp)

---
+++ problems/robot_bounded_in_circle/solution.cpp
@@ -0,0 +1,70 @@
+// class Solution {
+// public:
+//     bool isRobotBounded(string instructions) {
+//         int dir = 100;
+//         int up = 0;
+//         int side = 0;
+//         for(int j = 0;j<4;j++){
+//             for(int i = 0;i<instructions.length();i++){
+//                 if(instructions[i]=='G'){
+//                     if(dir%4==0){
+//                         up++;
+//                     }
+//                     else if(dir%4==1){
+//                         side++;
+//                     }
+//                     else if(dir%4==2){
+//                         up--;
+//                     }
+//                     else{
+//                         side--;
+//                     }
+//                 }
+//                 else if(instructions[i]=='L'){
+//                     dir--;
+//                 }
+//                 else{
+//                     dir++;
+//                 }
+//             }
+//             if(up==0 && side==0){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        int dir = 100;
+        int up = 0;
+        int side = 0;
+        for(int i = 0;i<instructions.length();i++){
+            if(instructions[i]=='G'){
+                if(dir%4==0){
+                    up++;
+                }
+                else if(dir%4==1){
+                    side++;
+                }
+                else if(dir%4==2){
+                    up--;
+                }
+                else{
+                    side--;
+                }
+            }
+            else if(instructions[i]=='L'){
+                dir--;
+            }
+            else{
+                dir++;
+            }
+        }
+        if((up==0 && side==0) || (dir%4!=0)){
+            return true;
+        }
+        return false;
+    }
+};
