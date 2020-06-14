b3ba73b40f980a921184c7375849a5308e7ca1e2
Manit Baser <49650773+manitbaser@users.noreply.github.com>
2020-06-14 04:31:36 +0000

    Sync LeetCode submission - Find the Distance Value Between Two Arrays (cpp)

--- problems/find_the_distance_value_between_two_arrays/solution.cpp
+++ problems/find_the_distance_value_between_two_arrays/solution.cpp
@@ -1,40 +1,5 @@
 class Solution {
 public:
-//     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-//         int a = 0;
-//         int count = 0;
-//         sort(arr2.begin(),arr2.end());
-//         sort(arr1.begin(),arr1.end());
-//         while(a<arr1.size() && arr1[a]<arr2[0]){
-//             if(arr2[0]-arr1[a]>d){
-//                 count++;
-//             }
-//             cout<<arr1[a]<<" "<<count<<endl;
-//             a++;
-//         }
-//         int b = 1;
-//         for(;a<arr1.size() && b<arr2.size();a++){
-//             while(b<arr2.size() && arr2[b]<arr1[a]){
-//                 b++;
-//             }
-//             if(b>=arr2.size()){
-//                 break;
-//             }
-//             if(arr1[a]-arr2[b-1]>d && arr2[b]-arr1[a]>d){
-//                 count++;
-//             }
-//             cout<<arr1[a]<<" "<<count<<endl;
-//         }
-//         b = arr2.size()-1;
-//         for(;a<arr1.size();a++){
-//             cout<<arr1[a]<<" "<<count<<endl;
-//             if(arr1[a]-arr2[b]>d){
-                
-//                 return count+arr1.size()-a;
-//             }
-//         }
-//         return count;
-//     }
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
         int n = arr1.size(), ans = 0;
         sort(arr2.begin(),arr2.end());
