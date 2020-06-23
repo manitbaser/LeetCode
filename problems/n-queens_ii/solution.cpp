2f20a56e3aa51a4fa37bf835276cff770378f7b6
Manit Baser <49650773+manitbaser@users.noreply.github.com>
2020-06-23 11:27:34 +0000

    Sync LeetCode submission - N-Queens II (cpp)

---
+++ problems/n-queens_ii/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int ans = 0;
+    void fn(vector<int> row,vector<int> d1,vector<int> d2,int n, int i){
+        if(i==n) ans++;
+        else 
+            for(int j = 0;j<n;j++)
+            if(!row[j] && !d1[i+j] && !d2[n-1+i-j]){
+                row[j] = true;
+                d1[i+j] = true;
+                d2[n-1+i-j] = true;
+                fn(row,d1,d2,n,i+1);
+                row[j] = false;
+                d1[i+j] = false;
+                d2[n-1+i-j] = false;
+            }
+    }
+                
+    int totalNQueens(int n) {
+        if(n<4) return (n==1)?1:0;
+        vector<int> row(n);
+        vector<int> d1(2*n-1);
+        vector<int> d2(2*n-1);
+        fn(row,d1,d2,n,0);
+        return ans;
+    }
+};
