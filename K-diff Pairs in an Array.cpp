b3713b85ccca2aca997659254db4171d158157ad
Manit Baser <49650773+manitbaser@users.noreply.github.com>
2020-06-23 09:53:36 +0530

    Create K-diff Pairs in an Array.cpp

---
+++ K-diff Pairs in an Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        if(k<0) return 0;
+        unordered_map<int,int> x;
+        for(int i : nums) x[i]++;
+        int ans = 0;
+        if(k==0) {for(auto itr : x) if(itr.second >1) ans++;}
+        else {for(auto itr : x) if(x.count(itr.first +k)) ans++;}
+        return ans;
+    }
+};
