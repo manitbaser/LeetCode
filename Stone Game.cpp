8306f4f219d8ce4eaa6d877d12353d9b95dad5ef
Manit Baser <49650773+manitbaser@users.noreply.github.com>
2020-07-02 09:05:21 +0530

    Update Stone Game.cpp

--- Stone Game.cpp
+++ Stone Game.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int val = 0;
-        int i = 0, j = piles.size()-1, flag = 1;
-        while(i<=j){
-            if(piles[i]>piles[j]) val+=flag*piles[i++];
-            else val+=flag*piles[j--];
-            flag*=-1;
-        }
-        return val!=0;
+        return true;
     }
 };
