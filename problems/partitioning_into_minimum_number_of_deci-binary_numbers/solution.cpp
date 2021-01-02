4ef67294f84c85429e64e5019afc7cb9f14ae5d4
Manit Baser <49650773+manitbaser@users.noreply.github.com>
2021-01-02 09:30:01 +0000

    Sync LeetCode submission - Partitioning Into Minimum Number Of Deci-Binary Numbers (cpp)

---
+++ problems/partitioning_into_minimum_number_of_deci-binary_numbers/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minPartitions(string n,int i = 0) {
+        int ans = 0;
+        for(int i = 0;i<n.length();i++) ans = max(ans, n[i]-'0');
+        return ans;
+    }
+};
