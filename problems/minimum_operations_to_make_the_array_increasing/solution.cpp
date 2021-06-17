a53a9e423ce4843463dc13162c64c788f7cd662c
Manit Baser <49650773+manitbaser@users.noreply.github.com>
2021-06-17 04:07:55 +0000

    Sync LeetCode submission - Minimum Operations to Make the Array Increasing (cpp)

---
+++ problems/minimum_operations_to_make_the_array_increasing/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 1;i<nums.size();i++)
+            ans+=max(0,nums[i-1]+1-nums[i]), nums[i] = max(nums[i], nums[i-1]+1);
+        return ans;
+    }
+};
